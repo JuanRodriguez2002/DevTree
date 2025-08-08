73a643e3b749c80c01d1400196de3d4266dc72f1
Juan Diego Rodriguez <rodriguezsalasjuandiego070@gmail.com>
2025-08-08 16:07:01 -0400

    actualizacion

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,7 +1,21 @@
 import User from "../models/User";
+import { Request, Response } from "express";
+import { hashPassword } from "../utils/auth";
 
-export const createAccount = async (req, res)=>{
+export const createAccount = async (req : Request, res: Response)=>{
+    
+    const { email, password } = req.body
+
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        const error = new Error('El Usuario ya esta registrado')
+        return res.status(409).json({error : error.message})
+    }
+    
+    
     const user = new User(req.body)
+    user.password = await hashPassword(password);
 
     await user.save();
     
